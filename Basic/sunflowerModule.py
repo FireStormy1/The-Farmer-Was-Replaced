fe3d8a2dd338c0ccbdc6906b4e3d0814a767eb99
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-06-07 19:11:24 +0530

    Create sunflowerModule.py

---
+++ Basic/sunflowerModule.py
@@ -0,0 +1,68 @@
+import tools
+import parameters
+import farmModule
+
+
+
+def plant_sunflowers():
+	allPetals = []
+	activeDrones = []
+	
+	for line in range(parameters.WORLD_SIZE):
+		if num_drones() == max_drones():
+			petalList = plant_sunflower_line()
+			for petalItem in petalList:
+				allPetals.append(petalItem)
+			continue
+		else:	
+			while not num_drones() < max_drones(): 
+				pass
+			activeDrones.append(spawn_drone(plant_sunflower_line))
+		move(North)
+			
+	for drone in activeDrones:
+		petalList = wait_for(drone)
+		for petalItem in petalList:
+			allPetals.append(petalItem)
+	return allPetals
+	
+
+def plant_sunflower_line():
+	petalList = []
+	for col in range(parameters.WORLD_SIZE):
+		if get_entity_type() != Entities.Sunflower:
+			tools.check_till()
+			plant(Entities.Sunflower)
+			petalNbr = measure()
+			if petalNbr > 7:
+				petalList.append([get_pos_x(),get_pos_y(),petalNbr])
+		move(East)
+	return petalList
+
+def harvest_sunflower(petalList):
+	activeDrones = []
+	
+	for i in range(15, 7, -1):
+		for drone in activeDrones:
+			wait_for(drone)
+		for coords in petalList:
+			if coords[2] == i:
+				if num_drones() == max_drones():
+					farmModule.go_to_harvest(coords)
+					continue
+				else:	
+					while not num_drones() < max_drones(): 
+						pass
+					activeDrones.append(spawn_drone(tools.drone_func(farmModule.go_to_harvest,coords)))
+
+def farm_sunflower():
+	tools.go_to(0,0)
+	
+	petalList = plant_sunflowers()
+	
+	tools.go_to(0,0)
+	while not can_harvest():
+		pass
+	
+	tools.go_to(0,0)
+	harvest_sunflower(petalList)
