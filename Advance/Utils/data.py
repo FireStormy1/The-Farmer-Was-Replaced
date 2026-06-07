b7ebbbc1b521d116370081cf8fcc092e0d10193d
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-06-07 18:35:14 +0530

    Create data.py

---
+++ Advance/Utils/data.py
@@ -0,0 +1,256 @@
+techList = [Unlocks.Auto_Unlock, Unlocks.Cactus, Unlocks.Carrots,
+			Unlocks.Costs, Unlocks.Debug, Unlocks.Debug_2,
+			Unlocks.Dictionaries, Unlocks.Dinosaurs, Unlocks.Expand,
+			Unlocks.Fertilizer, Unlocks.Functions, Unlocks.Grass, Unlocks.Hats,
+			Unlocks.Import, Unlocks.Leaderboard, Unlocks.Lists, Unlocks.Loops,
+			Unlocks.Mazes, Unlocks.Megafarm, Unlocks.Operators, Unlocks.Plant,
+			Unlocks.Polyculture, Unlocks.Pumpkins, Unlocks.Senses, Unlocks.Simulation,
+			Unlocks.Speed, Unlocks.Sunflowers, Unlocks.The_Farmers_Remains, Unlocks.Timing,
+			Unlocks.Top_Hat, Unlocks.Trees, Unlocks.Utilities, Unlocks.Variables, Unlocks.Watering]
+
+techData = {
+	"Loops": {
+		"object": Unlocks.Loops,
+		"max": 1
+	},
+	"Hats": {
+		"object": Unlocks.Hats,
+		"max": 1
+	},
+	"Speed": {
+		"object": Unlocks.Speed,
+		"max": 5
+	},
+	"Grass": {
+		"object": Unlocks.Grass,
+		"max": 10
+	},
+	"Expand": {
+		"object": Unlocks.Expand,
+		"max": 9
+	},
+	"Plant": {
+		"object": Unlocks.Plant,
+		"max": 1
+	},
+	"Carrots": {
+		"object": Unlocks.Carrots,
+		"max": 10
+	},
+	"Debug": {
+		"object": Unlocks.Debug,
+		"max": 1
+	},
+	"Operators": {
+		"object": Unlocks.Operators,
+		"max": 1
+	},
+	"Watering": {
+		"object": Unlocks.Watering,
+		"max": 9
+	},
+	"Trees": {
+		"object": Unlocks.Trees,
+		"max": 10
+	},
+	"Debug_2": {
+		"object": Unlocks.Debug_2,
+		"max": 1
+	},
+	"Timing": {
+		"object": Unlocks.Timing,
+		"max": 1
+	},
+	"Senses": {
+		"object": Unlocks.Senses,
+		"max": 1
+	},
+	"Variables": {
+		"object": Unlocks.Variables,
+		"max": 1
+	},
+	"Fertilizer": {
+		"object": Unlocks.Fertilizer,
+		"max": 4
+	},
+	"Sunflowers": {
+		"object": Unlocks.Sunflowers,
+		"max": 1
+	},
+	"Pumpkins": {
+		"object": Unlocks.Pumpkins,
+		"max": 10
+	},
+	"Simulation": {
+		"object": Unlocks.Simulation,
+		"max": 1
+	},
+	"Lists": {
+		"object": Unlocks.Lists,
+		"max": 1
+	},
+	"Functions": {
+		"object": Unlocks.Functions,
+		"max": 1
+	},
+	"Mazes": {
+		"object": Unlocks.Mazes,
+		"max": 6
+	},
+	"Cactus": {
+		"object": Unlocks.Cactus,
+		"max": 6
+	},
+	"Polyculture": {
+		"object": Unlocks.Polyculture,
+		"max": 5
+	},
+	"Leaderboard": {
+		"object": Unlocks.Leaderboard,
+		"max": 1
+	},
+	"Dictionaries": {
+		"object": Unlocks.Dictionaries,
+		"max": 1
+	},
+	"Import": {
+		"object": Unlocks.Import,
+		"max": 1
+	},
+	"Utilities": {
+		"object": Unlocks.Utilities,
+		"max": 1
+	},
+	"Top_Hat": {
+		"object": Unlocks.Top_Hat,
+		"max": 1
+	},
+	"Megafarm": {
+		"object": Unlocks.Megafarm,
+		"max": 5
+	},
+	"Dinosaurs": {
+		"object": Unlocks.Dinosaurs,
+		"max": 6
+	},
+	"Costs": {
+		"object": Unlocks.Costs,
+		"max": 1
+	},
+	"The_Farmers_Remains": {
+		"object": Unlocks.The_Farmers_Remains,
+		"max": 1
+	},
+	"Auto_Unlock": {
+		"object": Unlocks.Auto_Unlock,
+		"max": 1
+	}
+}
+
+techUnlock = [
+	{"name": "Speed", "finished": False, "level": 1},
+	{"name": "Plant", "finished": False, "level": 1},
+	{"name": "Expand", "finished": False, "level": 1},
+	{"name": "Expand", "finished": False, "level": 2},
+	{"name": "Speed", "finished": False, "level": 2},
+	{"name": "Carrots", "finished": False, "level": 1},
+	{"name": "Grass", "finished": False, "level": 2},
+	{"name": "Trees", "finished": False, "level": 1},
+	{"name": "Trees", "finished": False, "level": 2},
+	{"name": "Expand", "finished": False, "level": 3},
+	{"name": "Carrots", "finished": False, "level": 2},
+	{"name": "Speed", "finished": False, "level": 3},
+	{"name": "Expand", "finished": False, "level": 4},
+	{"name": "Watering", "finished": False, "level": 1},
+	{"name": "Watering", "finished": False, "level": 2},
+	{"name": "Carrots", "finished": False, "level": 3},
+	{"name": "Grass", "finished": False, "level": 3},
+	{"name": "Sunflowers", "finished": False, "level": 1},
+	{"name": "Fertilizer", "finished": False, "level": 1},
+	{"name": "Watering", "finished": False, "level": 3},
+	{"name": "Speed", "finished": False, "level": 4},
+	{"name": "Pumpkins", "finished": False, "level": 1},
+	{"name": "Watering", "finished": False, "level": 4},
+	{"name": "Polyculture", "finished": False, "level": 1},
+	{"name": "Speed", "finished": False, "level": 5},
+	{"name": "Expand", "finished": False, "level": 5},
+	{"name": "Fertilizer", "finished": False, "level": 2},
+	{"name": "Mazes", "finished": False, "level": 1},
+	{"name": "Megafarm", "finished": False, "level": 1},
+	{"name": "Trees", "finished": False, "level": 3},
+	{"name": "Trees", "finished": False, "level": 4},
+	{"name": "Carrots", "finished": False, "level": 4},
+	{"name": "Watering", "finished": False, "level": 5},
+	{"name": "Pumpkins", "finished": False, "level": 2},
+	{"name": "Pumpkins", "finished": False, "level": 3},
+	{"name": "Expand", "finished": False, "level": 6},
+	{"name": "Cactus", "finished": False, "level": 1},
+	{"name": "Dinosaurs", "finished": False, "level": 1},
+	{"name": "Dinosaurs", "finished": False, "level": 2},
+	{"name": "Polyculture", "finished": False, "level": 2},
+	{"name": "Mazes", "finished": False, "level": 2},
+	{"name": "Mazes", "finished": False, "level": 3},
+	{"name": "Megafarm", "finished": False, "level": 2},
+	{"name": "Megafarm", "finished": False, "level": 3},
+	{"name": "Grass", "finished": False, "level": 4},
+	{"name": "Trees", "finished": False, "level": 5},
+	{"name": "Fertilizer", "finished": False, "level": 3},
+	{"name": "Fertilizer", "finished": False, "level": 4},
+	{"name": "Watering", "finished": False, "level": 6},
+	{"name": "Carrots", "finished": False, "level": 5},
+	{"name": "Carrots", "finished": False, "level": 6},
+	{"name": "Pumpkins", "finished": False, "level": 4},
+	{"name": "Pumpkins", "finished": False, "level": 5},
+	{"name": "Expand", "finished": False, "level": 7},
+	{"name": "Megafarm", "finished": False, "level": 4},
+	{"name": "Cactus", "finished": False, "level": 2},
+	{"name": "Cactus", "finished": False, "level": 3},
+	{"name": "Dinosaurs", "finished": False, "level": 3},
+	{"name": "Dinosaurs", "finished": False, "level": 4},
+	{"name": "Dinosaurs", "finished": False, "level": 5},
+	{"name": "Mazes", "finished": False, "level": 4},
+	{"name": "Debug", "finished": False, "level": 1},
+	{"name": "Timing", "finished": False, "level": 1},
+	{"name": "Simulation", "finished": False, "level": 1},
+	{"name": "Leaderboard", "finished": False, "level": 1},
+	
+	{"name": "Grass", "finished": False, "level": 5},
+	{"name": "Polyculture", "finished": False, "level": 3},
+	{"name": "Grass", "finished": False, "level": 6},
+	{"name": "Polyculture", "finished": False, "level": 4},
+	{"name": "Trees", "finished": False, "level": 6},
+	{"name": "Carrots", "finished": False, "level": 7},
+	{"name": "Pumpkins", "finished": False, "level": 6},
+	{"name": "Trees", "finished": False, "level": 7},
+	{"name": "Expand", "finished": False, "level": 8},
+	{"name": "Carrots", "finished": False, "level": 8},
+	{"name": "Pumpkins", "finished": False, "level": 7},
+	{"name": "Grass", "finished": False, "level": 7},
+	{"name": "Watering", "finished": False, "level": 7},
+	{"name": "Cactus", "finished": False, "level": 4},
+	{"name": "Trees", "finished": False, "level": 8},
+	{"name": "Polyculture", "finished": False, "level": 5},
+	{"name": "Mazes", "finished": False, "level": 5},
+	{"name": "Grass", "finished": False, "level": 8},
+	{"name": "Watering", "finished": False, "level": 8},
+	{"name": "Watering", "finished": False, "level": 9},
+	{"name": "Pumpkins", "finished": False, "level": 8},
+	{"name": "Expand", "finished": False, "level": 9},
+	{"name": "Megafarm", "finished": False, "level": 5},
+	{"name": "Cactus", "finished": False, "level": 5},
+	{"name": "Grass", "finished": False, "level": 9},
+	{"name": "Trees", "finished": False, "level": 9},
+	{"name": "Trees", "finished": False, "level": 10},
+	{"name": "Carrots", "finished": False, "level": 9},
+	{"name": "Pumpkins", "finished": False, "level": 9},
+	{"name": "Cactus", "finished": False, "level": 6},
+	{"name": "Mazes", "finished": False, "level": 6},
+	{"name": "Dinosaurs", "finished": False, "level": 6},
+	{"name": "Carrots", "finished": False, "level": 10},
+	{"name": "Pumpkins", "finished": False, "level": 10},
+	{"name": "Grass", "finished": False, "level": 10},
+	{"name": "The_Farmers_Remains", "finished": False, "level": 1},
+	{"name": "Top_Hat", "finished": False, "level": 1},
+	
+
+]
