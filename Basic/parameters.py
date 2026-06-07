e4bd83d47a2dc1e1a8b68fab9ae1750c293a77d2
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-06-07 19:10:10 +0530

    Create parameters.py

---
+++ Basic/parameters.py
@@ -0,0 +1,71 @@
+WORLD_SIZE = get_world_size()
+PRIORITY_CROP = None
+DRONE_NUMBER = max_drones()
+
+PLANTS = [
+	{
+		"name":"sunflower",
+		"plant":Entities.Sunflower,
+		"item":Items.Power,
+		"target":100000,
+		"priority":7
+	},
+	{
+		"name":"hay",
+		"plant":Entities.Grass,
+		"item":Items.Hay,
+		"target":10000000000,
+		"priority":5
+	},
+	{
+		"name":"tree",
+		"plant":Entities.Tree,
+		"item":Items.Wood,
+		"target":100000000000,
+		"priority":5
+	},
+	{
+		"name":"carrot",
+		"plant":Entities.Carrot,
+		"item":Items.Carrot,
+		"target":100000000000000,
+		"priority":5
+	},
+	{
+		"name":"pumpkin",
+		"plant":Entities.Pumpkin,
+		"item":Items.Pumpkin,
+		"target":1000000,
+		"priority":4
+	},
+	{
+		"name":"cactus",
+		"plant":Entities.Cactus,
+		"item":Items.Cactus,
+		"target":10000000000,
+		"priority":4
+	},
+	{
+		"name":"bones",
+		"plant":Items.Bone,
+		"item":Items.Bone,
+		"target":1,  #00000000,
+		"priority":3
+	},
+	{
+		"name":"gold",
+		"plant":Items.Gold,
+		"item":Items.Gold,
+		"target":10000,
+		"priority":3
+	},
+
+]
+
+SUNFLOWER_CYCLE = 50
+
+
+def get_plant(value,index):
+	for plant in PLANTS:
+		if plant[index] == value:
+			return plant
