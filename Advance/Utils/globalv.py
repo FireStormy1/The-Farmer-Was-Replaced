1b588ef01d4d694b6c8f3fc3ca346b0763e0db25
Saswat Dixit <85784995+FireStormy1@users.noreply.github.com>
2026-06-07 18:34:55 +0530

    Create globalv.py

---
+++ Advance/Utils/globalv.py
@@ -0,0 +1,21 @@
+
+size = get_world_size()
+power = {"min": 1*size**2, "max": 10*size**2}
+dinosaur_map = 0.5
+leaderboard_mode = True
+# ------------Global Variables --------------
+poly_companions = {}
+poly_goal = {"item": Items.Hay, "cost": 999999999, "name": Entities.Grass}
+
+
+pumpkin_goal = 9999999999
+
+cactus_drones = 1
+cactus_goal = 999999999
+
+weirdSubstance_goal = 999999999
+
+maze_goal = 999999999
+maze_iteration_max = 300
+
+dinosaur_goal = 999999999
